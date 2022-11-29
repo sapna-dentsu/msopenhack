db2ae0ae515eda9f9b79197b5d1182c674932994
sapna-dentsu <sapna.upreti@dentsu.com>
2022-11-29 17:29:58 +0530

    Initi commit

---
+++ HttpTriggerChallengeSapna1/index.js
@@ -0,0 +1,32 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const method = req.method;
+    console.log(req)
+    if (method === 'GET') {
+        const productId = req.query.productId;
+        const responseMessage = productId
+            ? "The product name for your product id " + productId + " is Starfruit Explosion"
+            : "This HTTP triggered function executed successfully. Pass a productId in the query string";
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: responseMessage
+            };
+    
+    } else if (method === 'POST') {
+        const productId = req.body && req.body.productId;
+        const responseMessage = productId
+            ? "The product name for your product id " + productId + " is Starfruit Explosion and the description is This starfruit ice cream is out of this world!"
+            : "This HTTP triggered function executed successfully. Pass a productId in the request body for a personalized response.";
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: responseMessage
+            };   
+    } else {
+        context.res = {
+            status: 404,
+            body: "The request your are looking is not available"
+        };
+    }
+
+    
+}
